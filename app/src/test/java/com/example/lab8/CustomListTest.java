5854a0b526961b09aed632f99424f4e54730c3f3
“David <ashieze@ualberta.ca>
2023-03-08 17:47:25 -0700

    hascity Test should fail

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -2,6 +2,7 @@ package com.example.lab8;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 //import org.junit.Before;
@@ -35,6 +36,15 @@ public class CustomListTest {
         int listSize = list.getCount();
         list.addCity(new City("Estevan", "SK"));
         assertEquals(list.getCount(),listSize + 1);
+    }
+    public void testHasCity(){
+
+        list = MockCityList();
+        int listSize = list.getCount();
+        City city = new City("Yellowknife", "Northwest Territories");
+        list.addCity(city);
+        assertTrue(list.hasCity(city));
+
     }
 
 
